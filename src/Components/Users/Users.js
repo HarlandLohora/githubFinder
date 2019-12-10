f80693e1d18d54be5573b864a20b9fddf34320d1
Harland Lohora <lohoraharland@gmail.com>
2019-12-09 20:26:20 -0600

    Spinner component complete

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem';
+import Spinner from '../Layout/Spinner';
 
-class Users extends Component{
-    render( props ){
-        const userStyle = {
-            display: 'grid',
-            gridTemplateColumns: 'repeat(3,1fr)',
-            gridGap:'1rem'
-        }
-        let { users } = this.props;
+
+const Users = ({ users,loading }) => {
+    const userStyle = {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3,1fr)',
+        gridGap:'1rem'
+    }
+    if( loading ){
+        return <Spinner />
+    }else{
         return(
             <div style = { userStyle }>
                 {
